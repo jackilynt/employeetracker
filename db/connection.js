f4419988e7b0f9bb972bc4073e677ee1290dabca
Jackie Tan <jackilyn.tan@gmail.com>
2023-06-06 14:32:03 -0400

    jttest

--- db/connection.js
+++ db/connection.js
@@ -1,34 +1,34 @@
-// connection.js
-
-const mysql = require('mysql2');
-
-// Create a connection pool
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'sqlpassword',
-  database: 'employee_db',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
-// Get a connection from the pool
-const connection = pool.promise();
-
-module.exports = connection;
-
 // // connection.js
 
 // const mysql = require('mysql2');
 
-// const connection = mysql.createConnection({
+// // Create a connection pool
+// const pool = mysql.createPool({
 //   host: 'localhost',
-//   port: 3306,
 //   user: 'root',
-//   password: 'your_mysql_password',
-//   database: 'employee_tracker',
+//   password: 'sqlpassword',
+//   database: 'employee_db',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
 // });
 
+// // Get a connection from the pool
+// const connection = pool.promise();
+
 // module.exports = connection;
+
+// connection.js
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'sqlpassword',
+  database: 'employee_db',
+});
+
+module.exports = connection;
 
